5c98850dd1fa136bcf9631d578f0542bdf672a65
= <=>
2018-03-08 04:00:31 +0530

    Hello flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(new MaterialApp(
-    home: null,
+    home: new MyApp(),
   ));
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return new Material(
+      color: Colors.blueGrey,
+      child: new InkWell(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Hello Flutter", style: new TextStyle(color: Colors.white,),),
+          ],
+        ),
+      ),
+    );
+  }
 }
